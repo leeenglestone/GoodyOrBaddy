ae3d2d58a5b1f8abacb12ee106413adf04f105c8
Lee Englestone <lee.englestone@gmail.com>
2021-09-11 21:43:50 +0100

    Authorisation level set to anonymous

--- src/ImageClassification.Solution/ImageClassification.AzureFunction/Function1.cs
+++ src/ImageClassification.Solution/ImageClassification.AzureFunction/Function1.cs
@@ -15,7 +15,7 @@ namespace ImageClassification.AzureFunction
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log, ExecutionContext ctx)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
